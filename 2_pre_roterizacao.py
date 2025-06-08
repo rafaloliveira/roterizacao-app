9f1ddf7ef572f079269cf437ff9ca30c0bed1504
Rafael Oliveira <103541128+rafaloliveira@users.noreply.github.com>
2025-06-07 22:11:11 -0300

    Descreva aqui o que foi alterado

--- 2_pre_roterizacao.py
+++ 2_pre_roterizacao.py
@@ -114,7 +114,7 @@ def controle_selecao(chave_estado, df_todos, grid_key, grid_options):
     df_todos,
     gridOptions=grid_options,
     update_mode=GridUpdateMode.SELECTION_CHANGED,
-    height=600,
+    height=200,
     use_container_width=True,  # <-- Adicionado
     allow_unsafe_jscode=True,
     key=grid_key
@@ -483,7 +483,7 @@ def criar_grid_destacado(df, key, selection_mode="multiple", page_size=500, altu
         gridOptions=grid_options,
         update_mode=GridUpdateMode.SELECTION_CHANGED,
         fit_columns_on_grid_load=False,
-        height=600,
+        height=200,
         allow_unsafe_jscode=True,
         key=key
     )
@@ -1574,7 +1574,7 @@ def pagina_rotas_confirmadas():
                     gridOptions=grid_options,
                     update_mode=GridUpdateMode.SELECTION_CHANGED,
                     fit_columns_on_grid_load=True,
-                    height=600,
+                    height=200,
                     allow_unsafe_jscode=True
                 )
 
