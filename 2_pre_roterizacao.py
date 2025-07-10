072d9118dcd8e1d93bb2c8e84725cd145e56cd29
Rafael Oliveira <103541128+rafaloliveira@users.noreply.github.com>
2025-07-10 10:16:17 -0300

    Atualização

--- 2_pre_roterizacao.py
+++ 2_pre_roterizacao.py
@@ -4660,7 +4660,7 @@ def pagina_cargas_fechadas():
                     df_formatado,
                     gridOptions=grid_options,
                     update_mode=GridUpdateMode.SELECTION_CHANGED,
-                    fit_columns_on_grid_load=False,
+                    fit_columns_on_grid_load=True,
                     width="100%",
                     height=400,
                     allow_unsafe_jscode=True,
