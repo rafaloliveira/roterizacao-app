f8296cede47df4ac54166fa72c2b9ac2df7dafef
Rafael Oliveira <103541128+rafaloliveira@users.noreply.github.com>
2025-07-22 22:17:56 -0300

    Atualização

--- 2_pre_roterizacao.py
+++ 2_pre_roterizacao.py
@@ -2032,7 +2032,7 @@ def pagina_confirmar_producao():
                 grid_response = AgGrid(
                     df_formatado,
                     gridOptions=grid_options,
-                    update_mode=GridUpdateMode.NO_UPDATE, #  acompanhar teste 
+                    update_mode=GridUpdateMode.MODEL_CHANGED,  #  acompanhar teste 
                     fit_columns_on_grid_load=False,
                     width="100%",
                     height=400,
